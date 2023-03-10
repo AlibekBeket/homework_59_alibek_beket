35c25edec2c938f9ec39b932d66a6f8c88e483da
Alibek Beket <alibek1beket@gmail.com>
2023-03-10 12:02:07 +0600

    перерписал представления для редактирования, детального просмотра и удаления задач на джанговские UpdateView, DetailView и DeleteView, соответственно

--- app/issue_tracker/views/issue_tracker.py
+++ app/issue_tracker/views/issue_tracker.py
@@ -1,7 +1,8 @@
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse, reverse_lazy
 from django.utils.http import urlencode
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView, DetailView, DeleteView
 from issue_tracker.models import Issue, Status, Type
 
 from issue_tracker.forms import IssueForm
@@ -47,31 +48,18 @@ class IssueTrackerView(ListView):
         return context
 
 
-class IssueDetailView(TemplateView):
+class IssueDetailView(DetailView):
     template_name = 'issue_detail_page.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['issue'] = get_object_or_404(Issue, pk=kwargs['pk'])
-        return context
+    model = Issue
 
 
-class IssueUpdateView(TemplateView):
+class IssueUpdateView(UpdateView):
     template_name = 'issue_update_page.html'
+    form_class = IssueForm
+    model = Issue
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['issue'] = get_object_or_404(Issue, pk=kwargs['pk'])
-        context['form'] = IssueForm(instance=context['issue'])
-        return context
-
-    def post(self, request, *args, **kwargs):
-        issue = get_object_or_404(Issue, pk=kwargs['pk'])
-        form = IssueForm(request.POST, instance=issue)
-        if form.is_valid():
-            form.save()
-            return redirect('issue_detail', pk=issue.pk)
-        return render(request, 'issue_update_page.html', context={'form': form, 'issue': issue})
+    def get_success_url(self):
+        return reverse('issue_detail', kwargs={'pk': self.object.pk})
 
 
 class IssueAddView(TemplateView):
@@ -90,15 +78,8 @@ class IssueAddView(TemplateView):
         return render(request, 'issue_create_page.html', context={'form': form})
 
 
-class IssueDeleteView(TemplateView):
+class IssueDeleteView(DeleteView):
     template_name = 'issue_delete_page.html'
+    model = Issue
+    success_url = reverse_lazy('issues_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['issue'] = get_object_or_404(Issue, pk=kwargs['pk'])
-        return context
-
-    def post(self, request, *args, **kwargs):
-        issue = get_object_or_404(Issue, pk=kwargs['pk'])
-        issue.delete()
-        return redirect('issues_list')
