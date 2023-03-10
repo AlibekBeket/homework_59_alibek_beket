245d82f4e700cca59a379249885eb2b44ef8b4ad
Alibek Beket <alibek1beket@gmail.com>
2023-03-10 14:35:27 +0600

    добавил страницу для просмотра всех проектов

--- app/issue_tracker/urls.py
+++ app/issue_tracker/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from issue_tracker.views.issue_tracker import IssueTrackerView, IssueDetailView, IssueUpdateView, IssueAddView, \
     IssueDeleteView
 
+from issue_tracker.views.project import ProjectListView
+
 urlpatterns = [
     path('', IssueTrackerView.as_view()),
     path('issue_tracker/', IssueTrackerView.as_view(), name='issues_list'),
     path('issue_tracker/<int:pk>', IssueDetailView.as_view(), name='issue_detail'),
     path('issue_tracker/<int:pk>/update', IssueUpdateView.as_view(), name='issue_update'),
     path('issue_tracker/add', IssueAddView.as_view(), name='issue_add'),
-    path('issue_tracker/<int:pk>/delete', IssueDeleteView.as_view(), name='issue_delete')
+    path('issue_tracker/<int:pk>/delete', IssueDeleteView.as_view(), name='issue_delete'),
+    path('project/', ProjectListView.as_view(), name='projects_list')
 ]
