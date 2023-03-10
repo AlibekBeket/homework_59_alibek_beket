d6f973246e840ebc5aba60a1aa783efde0c259a8
Alibek Beket <alibek1beket@gmail.com>
2023-03-10 16:50:43 +0600

    убрал лишние пути в urls и стелизовал главную страницу с проектами

--- app/issue_tracker/urls.py
+++ app/issue_tracker/urls.py
@@ -7,12 +7,7 @@ from issue_tracker.views.project import ProjectListView
 
 
 urlpatterns = [
-    path('', IssueTrackerView.as_view()),
-    path('issue_tracker/', IssueTrackerView.as_view(), name='issues_list'),
-    path('issue_tracker/<int:pk>', IssueDetailView.as_view(), name='issue_detail'),
-    path('issue_tracker/<int:pk>/update', IssueUpdateView.as_view(), name='issue_update'),
-    path('issue_tracker/add', IssueAddView.as_view(), name='issue_add'),
-    path('issue_tracker/<int:pk>/delete', IssueDeleteView.as_view(), name='issue_delete'),
+    path('', ProjectListView.as_view()),
     path('project/', ProjectListView.as_view(), name='projects_list'),
     path('project/<int:project_pk>', IssueTrackerView.as_view(), name='project_detail'),
     path('project/<int:project_pk>/add', IssueAddView.as_view(), name='project_issue_add'),
